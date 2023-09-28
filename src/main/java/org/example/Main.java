eccda78d2a8c6388c4f4431810f10efdb77061bc
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-28 11:23:34 +0200

    Instantiate mediaController and play music and video

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MediaController mediaController = new MediaController();
+        VideoPlayer videoPlayer1 = new VideoPlayer();
+        MusicPlayer musicPlayer1 = new MusicPlayer();
+
+
+        mediaController.playMedia(videoPlayer1);
+        //mediaController.playMedia(new VideoPlayer());
+        mediaController.playMedia(musicPlayer1);
     }
 }
