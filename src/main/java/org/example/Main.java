e253c9c78c24ad38a9babaa5790a797573735196
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-28 11:25:40 +0200

    Instantiate MusicPLayer and VideoPlayer using the Interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         MediaController mediaController = new MediaController();
-        VideoPlayer videoPlayer1 = new VideoPlayer();
-        MusicPlayer musicPlayer1 = new MusicPlayer();
+        Playable videoPlayer1 = new VideoPlayer();
+        Playable musicPlayer1 = new MusicPlayer();
 
 
         mediaController.playMedia(videoPlayer1);
